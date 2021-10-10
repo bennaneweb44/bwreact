c714a6d97510b2b6e1f4dc491dda5ce785a6e0a5
bennaneweb44 <58911331+bennaneweb44@users.noreply.github.com>
2021-10-10 14:37:12 +0200

    Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,24 @@
-var path = require('path');
-var config = {
-    entry: [path.resolve(__dirname, 'src/index.js')],
-    output: {
-        path: path.resolve(__dirname, 'public/build'),
-        filename: 'bundle.js'
-    },
-    resolve: {
-        extensions: ['', '.js', '.jsx']
-    },
-    module: {
-        loaders: [{
-            test: /.jsx?$/,
-            loader: 'babel-loader',
-            exclude: /node_modules/,
-            query: {
-                presets: ['es2015', 'react']
-            }
-        }]
-    }
+const path = require('path');
+ 
+module.exports = {
+  entry: path.resolve(__dirname, './src/index.js'),
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js']
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, './dist'),
+  },
 };
-module.exports = config;
