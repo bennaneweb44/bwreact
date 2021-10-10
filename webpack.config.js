fed0112ab97a70b0067caa0eec8c90dd8c67e165
bennaneweb44 <58911331+bennaneweb44@users.noreply.github.com>
2021-10-10 14:26:51 +0200

    install webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,22 @@
-const path = require('path');
-
-module.exports = {
-  entry: path.resolve(__dirname, './src/index.js'),
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ['babel-loader'],
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['*', '.js', '.jsx'],
-  },
-  output: {
-    path: path.resolve(__dirname, './public'),
-    filename: 'bundle.js',
-  },
-  devServer: {
-    contentBase: path.resolve(__dirname, './public'),
-  },
+var path = require('path');
+var config = {
+    entry: [path.resolve(__dirname, 'src/main.jsx')],
+    output: {
+        path: path.resolve(__dirname, 'public/build'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    module: {
+        loaders: [{
+            test: /.jsx?$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            query: {
+                presets: ['es2015', 'react']
+            }
+        }]
+    }
 };
+module.exports = config;
