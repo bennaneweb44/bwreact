b720b006870fad72feca5e17b60cfd4ccc4d9f38
bennaneweb44 <58911331+bennaneweb44@users.noreply.github.com>
2021-10-18 06:04:59 +0200

    Email sending 1st version

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,41 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Axios from 'axios'
+import Validator from 'validator'
 import '../App.scss';
 
-window.addEventListener('click', function(e) {
-  if (document.getElementById('sendEmail').contains(e.target)) {
-    const headers = {
-      'Content-Type': 'application/json'
-    }
-
-    let data = {
-      'name': 'aziz',
-      'telephone': '0760715758',
-      'email': 'aziz44000@yahoo.fr',
-      'message': 'ca va'
-    }
-    
-    Axios.post('https://services.bennaneweb.fr/email/send', data, {
-        headers: headers
-    })
-      .then((response) => {
-        console.log('its ok ....');
-        console.log(response)
-        //TODO: toast here
-        //TODO: toast here
-        //TODO: toast here
-        //ok
-    })
-    .catch((error) => {
-      //TODO: toast here
-      //TODO: toast here
-      //TODO: toast here
-      console.error('ERROR ....'+error.message);
-    })
-  }
-})
-
 function Contact() {
+  const [name, setName] = useState('')
+  const [telephone, setTelephone] = useState('')
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
+  const [emailSent, setEmailSent] = useState(false)
+
+  const submit = () => {
+    if (email && !Validator.isEmail(email)) {
+      alert('Erreur : l\'adresse email est invalide !')
+    }
+    else if (telephone && !Validator.isNumeric(telephone, 'fr-FR')) {
+      alert('Erreur : le téléphone est invalide !')
+    }
+    else if (name && email && Validator.isEmail(email) && telephone && message) {
+      const headers = {
+        'Content-Type': 'application/json'
+      }
+
+      let data = {
+        nom: name,
+        telephone: telephone,
+        email: email,
+        message: message
+      }
+      
+      Axios.post('http://127.0.0.1:8000/email/send', data, {
+          headers: headers
+      })
+        .then((response) => {
+          console.log('its ok ....');
+          setName('');
+          setTelephone('');
+          setEmail('');
+          setMessage('');
+          setEmailSent(true);
+          //TODO: toast here
+      })
+      .catch((error) => {
+        //TODO: toast here
+        console.error('ERROR ....'+error.message);
+      })
+    } else {
+        alert('Au moins un des champs n\'est pas renseigné !');
+    }
+  }
+
   return (    
     <div>
       <div className="services d-flex text-light align-items-center" id="contact">
@@ -51,25 +65,28 @@ function Contact() {
                         <div className="row" >
                             <div className="col-lg-6">
                                 <div className="form-group py-2">
-                                    <input type="text" className="form-control form-control-input" id="exampleFormControlInput1" placeholder="Nom" />
+                                    <input type="text" className={`form-control form-control-input border border-default`} value={name} onChange={e => setName(e.target.value)} placeholder="Nom" />
                                 </div>                                
                             </div>
                             <div className="col-lg-6">
                                 <div className="form-group py-2">
-                                    <input type="email" className="form-control form-control-input" id="exampleFormControlInput2" placeholder="Numéro de téléphone" />
+                                    <input type="email" className={`form-control form-control-input border border-default`} value={telephone} onChange={e => setTelephone(e.target.value)} placeholder="Numéro de téléphone" />
                                 </div>                                 
                             </div>
                         </div>
                         <div className="form-group py-1">
-                            <input type="email" className="form-control form-control-input" id="exampleFormControlInput3" placeholder="E-mail" />
+                            <input type="email" className={`form-control form-control-input border border-default`} value={email} onChange={e => setEmail(e.target.value)} placeholder="E-mail" />
                         </div>  
                         <div className="form-group py-2">
-                            <textarea className="form-control form-control-input" id="exampleFormControlTextarea1" rows="6" placeholder="Message"></textarea>
+                            <textarea className={`form-control form-control-input border border-default`} value={message} onChange={e => setMessage(e.target.value)} rows="6" placeholder="Message"></textarea>
                         </div>                            
                     </div>
                     <div className="my-3">
-                        <button className="btn form-control-submit-button" id="sendEmail">Envoyer</button>
+                        <button onClick={submit} className="btn form-control-submit-button">Envoyer</button>
                     </div>
+                    <div className={`my-3 bg-success text-center rounded mt-4 border border-white ${emailSent ? "visible" : "d-none"}`} >
+                        <span><i className="fa fa-check"></i> Demande reçue. Je vous contacterai dès que possible !</span>
+                    </div>                    
                 </div>
             </div>
             <div className="col-lg-6 d-flex align-items-center" data-aos="fade-down">
