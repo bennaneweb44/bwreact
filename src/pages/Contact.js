42dcd0bbb6c7a72081d4d2f5bb78302279a3016c
bennaneweb44 <58911331+bennaneweb44@users.noreply.github.com>
2021-10-19 20:50:57 +0200

    Test validité form contact + toast

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,21 +1,62 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Axios from 'axios'
 import Validator from 'validator'
 import '../App.scss';
 
-function Contact() {
+const ToastErrorStyle = { 
+  color: '#ff7562', 
+  border: '1px solid #ff7562' 
+}
+
+const ToastSuccessStyle = { 
+  color: '#94ebb8', 
+  border: '1px solid #94ebb8' 
+}
+
+function Contact() { 
+
+  // Form
   const [name, setName] = useState('')
   const [telephone, setTelephone] = useState('')
   const [email, setEmail] = useState('')
   const [message, setMessage] = useState('')
   const [emailSent, setEmailSent] = useState(false)
 
-  const submit = () => {
+  // Toasts
+  const [toastStyle, setToastStyle] = useState(ToastErrorStyle)
+  const [toastActive, setToastActive] = useState(false)
+
+  const showToast = (message, withError) => {
+    setToastActive(true)
+
+    withError ? setToastStyle(ToastErrorStyle) : setToastStyle(ToastSuccessStyle)
+
+    toast(message, {
+      onClose: () => {
+        setToastActive(false)
+      },
+      theme: 'dark',
+      hideProgressBar: true,
+      autoClose: 2000
+    })
+  }
+  
+  const submit = (e) => {
+    e.preventDefault()
+
     if (email && !Validator.isEmail(email)) {
-      alert('Erreur : l\'adresse email est invalide !')
+      if (!toastActive) {
+        !toastActive && showToast('L\'adresse e-mail est invalide !', true)        
+      }      
+      return
     }
     else if (telephone && !Validator.isNumeric(telephone, 'fr-FR')) {
-      alert('Erreur : le téléphone est invalide !')
+      if (!toastActive) {
+        !toastActive && showToast('Le téléphone est invalide !', true)
+      }      
+      return
     }
     else if (name && email && Validator.isEmail(email) && telephone && message) {
       const headers = {
@@ -33,20 +74,19 @@ function Contact() {
           headers: headers
       })
         .then((response) => {
-          console.log('its ok ....');
           setName('');
           setTelephone('');
           setEmail('');
           setMessage('');
           setEmailSent(true);
-          //TODO: toast here
+
+          !toastActive && showToast('Demande reçue. Je vous contacterai bientôt.', false)
       })
       .catch((error) => {
-        //TODO: toast here
-        console.error('ERROR ....'+error.message);
+        !toastActive && showToast('Une erreur interne s\'est produite !', true)
       })
     } else {
-        alert('Au moins un des champs n\'est pas renseigné !');
+      !toastActive && showToast('Au moins un des champs est vide !', true)
     }
   }
 
@@ -54,6 +94,7 @@ function Contact() {
     <div>
       <div className="services d-flex text-light align-items-center" id="contact">
         <div className="container mt-5">
+          <ToastContainer toastStyle={toastStyle} />
           <div className="row pt-6">
             <div className="col-lg-6 d-flex align-items-center px-lg-7" data-aos="fade-right">
                 <div className="w-90 mr-0">
@@ -62,34 +103,44 @@ function Contact() {
                         <p className="para-light">Une questions, une demande d'informations ? </p>
                     </div>
                     <div>
-                        <div className="row" >
-                            <div className="col-lg-6">
-                                <div className="form-group py-2">
-                                    <input type="text" className={`form-control form-control-input border border-default`} value={name} onChange={e => setName(e.target.value)} placeholder="Nom" />
-                                </div>                                
-                            </div>
-                            <div className="col-lg-6">
-                                <div className="form-group py-2">
-                                    <input type="email" className={`form-control form-control-input border border-default`} value={telephone} onChange={e => setTelephone(e.target.value)} placeholder="Numéro de téléphone" />
-                                </div>                                 
-                            </div>
+                      <div className="row" >
+                        <div className="col-md-12">
+                          <div className={`my-3 bg-success text-center rounded mt-4 border border-white ${emailSent && 1==2 ? "visible" : "d-none"}`} >
+                            <span><i className="fa fa-check"></i> Demande reçue. Je vous contacterai bientôt !</span>
+                          </div> 
                         </div>
-                        <div className="form-group py-1">
-                            <input type="email" className={`form-control form-control-input border border-default`} value={email} onChange={e => setEmail(e.target.value)} placeholder="E-mail" />
+                        <div className="col-md-6">
+                          <div className="form-group py-2">
+                            <input type="text" className={`form-control form-control-input border border-default`} value={name} onChange={e => setName(e.target.value)} placeholder="Nom" />
+                          </div>                                
+                                </div>                                
+                          </div>                                
+                        </div>
+                        <div className="col-md-6">
+                          <div className="form-group py-2">
+                            <input type="email" className={`form-control form-control-input border border-default`} value={telephone} onChange={e => setTelephone(e.target.value)} placeholder="Numéro de téléphone" />
+                          </div>                                 
+                                </div>                                 
+                          </div>                                 
+                        </div>
+                      </div>
+                      <div className="form-group py-1">
+                        <input type="email" className={`form-control form-control-input border border-default`} value={email} onChange={e => setEmail(e.target.value)} placeholder="E-mail" />
+                      </div>  
                         </div>  
-                        <div className="form-group py-2">
-                            <textarea className={`form-control form-control-input border border-default`} value={message} onChange={e => setMessage(e.target.value)} rows="6" placeholder="Message"></textarea>
+                      </div>  
+                      <div className="form-group py-2">
+                        <textarea className={`form-control form-control-input border border-default`} value={message} onChange={e => setMessage(e.target.value)} rows="6" placeholder="Message"></textarea>
+                      </div>                            
                         </div>                            
+                      </div>                            
                     </div>
                     <div className="my-3">
-                        <button onClick={submit} className="btn form-control-submit-button">Envoyer</button>
-                    </div>
-                    <div className={`my-3 bg-success text-center rounded mt-4 border border-white ${emailSent ? "visible" : "d-none"}`} >
-                        <span><i className="fa fa-check"></i> Demande reçue. Je vous contacterai dès que possible !</span>
-                    </div>                    
+                        <button onClick={submit} disabled={toastActive} className="btn form-control-submit-button">Envoyer</button>
+                    </div>             
                 </div>
             </div>
-            <div className="col-lg-6 d-flex align-items-center" data-aos="fade-down">
+            <div className="col-md-6 d-flex align-items-center" data-aos="fade-down">
               <img className="img-fluid d-none d-lg-block" src="./assets/images/contact.jpg" alt="contact" />        
             </div>
           </div>
